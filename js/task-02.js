26d36e8f33341c57bf57d65b05fb5d4d272c7573
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-07-11 00:55:53 +0300

    make tast 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredients = document.querySelector('#ingredients');
+
+const createLi = (count => {
+
+  for (let i = 0; i < count; i += 1){
+    const li = document.createElement('li');
+
+    li.textContent = `${ingredients[i]}`;
+    li.classList.add('item');
+    listOfIngredients.append(li);
+  }
+})
+
+createLi(ingredients.length);
+
