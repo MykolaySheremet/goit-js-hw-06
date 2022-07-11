a5477721e4ac30c979e922522299882464582e41
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-07-12 01:02:08 +0300

    make task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formElement = document.querySelector('.login-form');
 
+formElement.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if (!event.currentTarget.elements.email.value|| 
+        !event.currentTarget.elements.password.value) {
+  
+        return alert('для входу - ВСІ ПОЛЯ повинні бути заповненні !!!');
+    }
+
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+     
+    const valuesDataForm = {
+        email,
+        password
+    };
+    console.log(valuesDataForm);
+    formElement.reset();
+});
